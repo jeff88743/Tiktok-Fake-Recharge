6e367148fb9e6c53cc31509d1c412eac47901838
zexceed <zexceed12300@gmail.com>
2023-07-21 20:52:00 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,15 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
+    <Router>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/coin' element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
